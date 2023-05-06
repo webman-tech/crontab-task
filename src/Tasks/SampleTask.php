f96a6452d0654a8f03bfd4a9fed077e9c38d8c68
kriss <462679766@qq.com>
2023-05-06 23:44:47 +0800

    docs: update

--- src/Tasks/SampleTask.php
+++ src/Tasks/SampleTask.php
@@ -3,6 +3,7 @@
 namespace WebmanTech\CrontabTask\Tasks;
 
 use WebmanTech\CrontabTask\BaseTask;
+use WebmanTech\CrontabTask\Exceptions\TaskException;
 
 class SampleTask extends BaseTask
 {
@@ -11,6 +12,15 @@ class SampleTask extends BaseTask
      */
     public function handle()
     {
-        echo date('Y-m-d H:i:s') . PHP_EOL;
+        echo date('Y-m-d H:i:s') . ' Test task' . PHP_EOL;
+
+        if (random_int(1, 100) > 50) {
+            throw new TaskException('该异常会记录 warning 日志');
+        }
+        if (random_int(1, 100) > 50) {
+            throw new \Exception('其他异常会记录 error 日志，不会抛出异常到 console');
+        }
+        $this->log('这个可以记录 info 日志');
+        // 正常结束无需返回
     }
 }
--- src/config/plugin/webman-tech/crontab-task/app.php
+++ src/config/plugin/webman-tech/crontab-task/app.php
@@ -3,6 +3,10 @@
 return [
     'enable' => true,
     'log' => [
+        /**
+         * @see \WebmanTech\CrontabTask\BaseTask
+         * @see \WebmanTech\CrontabTask\Tasks\SampleTask
+         */
         'channel' => 'default',
         'level' => 'info',
         'log_class' => true,
